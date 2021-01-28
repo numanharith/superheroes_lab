1f63c26565176e4cfb4673af74cf9dcbc4d7367f
numan <numan.h.99@gmail.com>
2021-01-28 09:48:39 +0800

    modified get route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 // index route
-app.get('/superheroes', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {
         superheroes: superheroes,
         villains: villains
